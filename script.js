9a7466b4b049c4eebeb814bd44907e0f732df287
Gabriel C <gaba.edits@skiff.com>
2023-05-31 18:35:11 +0300

    Update

--- script.js
+++ script.js
@@ -25,3 +25,43 @@ var elements = document.getElementsByClassName('emoji-container');
 for (var i = 0; i < elements.length; i++) {
   twemoji.parse(elements[i]);
 }
+
+        fetch('https://api.boekestijntransport.com:50125/api/get_trucks')
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                appendData(data);
+                                console.log('data: ' + data['data']);
+            })
+            .catch(function (err) {
+                console.log('error: ' + err);
+            });
+        function appendData(data) {
+            var mainContainer = document.getElementById("myData");
+            var div = document.createElement("div");
+            div.innerHTML = data['data'];
+            mainContainer.appendChild(div);
+        }
+
+        function scrollToTop() {
+          window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+          });
+        }
+    
+        function scrollToLocation() {
+          window.scrollTo({
+            top: 2000,
+            behavior: 'smooth'
+          });
+        }
+    
+        function scrollToCall() {
+          window.scrollTo({
+            top: 6000,
+            behavior: 'smooth'
+          });
+        }
+    
