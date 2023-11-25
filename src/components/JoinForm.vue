297e765b99cd26c04264adc96f3ccc5fce755289
Gabriel Cozma <me@gabs.eu.org>
2023-11-25 22:22:15 +0200

    Include All Files
    Git..

---
+++ src/components/JoinForm.vue
@@ -0,0 +1,105 @@
+<template>
+  <div>
+      <div class="popup-overlay">
+      <div class="popup-content">
+      <a @click="closePopup" class="close-button">×</a>
+      <form ref="myForm" @submit.prevent="sendMessageToTelegram" class="form-container">
+    <h1 class="form-title">{{ $t("mainarea.joinus") }}</h1>
+    <div class="form-label">
+      <p id="form-desc">{{ $t("form.desc") }}</p>
+      <br><p><sup id="form-sup"><b>{{ $t("form.note") }} *</b></sup></p>
+    </div>
+    <div class="form-group">
+      <label for="name">{{ $t("form.name") }} *</label>
+      <input type="text" id="name" class="form-control" required v-model="name" placeholder="Сергей Петров">
+    </div>
+    <div class="form-group">
+      <label for="tel">{{ $t("form.tel") }} *</label>
+      <input type="tel" id="tel" class="form-control" required v-model="tel" placeholder="+373xxxxxxxx">
+    </div>
+    <div class="form-group">
+      <label for="email">{{ $t("form.email") }} *</label>
+      <input type="email" id="email" class="form-control" required v-model="email" placeholder="example@example.com">
+    </div>
+    <div class="form-group">
+      <label for="exp">{{ $t("form.exp") }} *</label>
+      <input type="text" id="exp" class="form-control" required v-model="exp" placeholder="0, 1, 2...">
+    </div>
+    <div class="form-group">
+      <label for="self">{{ $t("form.about") }}</label>
+      <textarea id="self" class="form-control" rows="3" v-model="self"></textarea>    </div>
+    <div id="success" class="alert alert-success mt-3" role="alert"></div>
+    <button type="submit" class="btn btn-primary mt-4">Submit</button>
+  </form>
+        </div>
+        </div>
+        </div>
+</template>
+
+
+<script>
+import axios from 'axios';
+
+export default {
+  mounted() {
+    document.addEventListener('keydown', this.handleEscKey);
+  },
+
+  beforeDestroy() {
+    document.removeEventListener('keydown', this.handleEscKey);
+  },
+  data() {
+    return {
+      name: '',
+      tel: '',
+      email: '',
+      exp: '',
+      self: '',
+    };
+  },
+  methods: {
+    async sendMessageToTelegram() {
+      try {
+        const TOKEN = import.meta.env.VITE_TELEGRAM_TOKEN;
+        const CHAT_ID = '-1001627354708';
+        const URI_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+
+        const message = `
+          <b>Заявка с сайта soferii.md</b>
+          <b>Sender:</b> ${this.name}
+          <b>GSM:</b> ${this.tel}
+          <b>Email:</b> ${this.email}
+          <b>Experience:</b> ${this.exp}
+          <b>About me:</b> ${this.self}
+        `;
+
+        const response = await axios.post(URI_API, {
+          chat_id: CHAT_ID,
+          parse_mode: 'html',
+          text: message,
+        });
+
+        console.log('Telegram API response:', response.data);
+
+        this.name = '';
+        this.tel = '';
+        this.email = '';
+        this.exp = '';
+        this.self = '';
+
+        this.$router.push('/')
+      } catch (error) {
+        console.error('Error sending message:', error);
+      }
+    },
+    closePopup() {
+      this.$router.push('/');
+    },
+        handleEscKey(event) {
+      if (event.key === 'Escape') {
+        this.closePopup();
+      }
+    },
+  },
+};
+</script>
---
+++ src/components/WebsiteInformation.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="popup-overlay">
+  <div class="popup-content">
+  <a @click="closeInfoPopup" class="close-button">×</a>
+  <div class="info-popup">
+    <!-- Website Information -->
+    <div class="info-title">Website Information</div>
+    <div class="info-description">
+      This website has been developed using Vue.js and Vite and is an integral part of the <a href="https://gabs.eu.org/aeolus"><b>Aeolus</b></a> project. The Framework's structure follows the <a href="https://semantichtml.github.io">Semantic HTML Convention</a> and an Component-Based Architecture.
+      <br><br>Furthermore, this website is fully <b>open source</b>. You can access it's source code <a href="https://github.com/GabsEdits/boekestijn">here</a>.
+
+        <br><br>
+    </div>
+    <div>
+    </div>
+
+    <!-- Build Number -->
+    <div class="build-number">
+      <h1>Build Number</h1>
+      <p>This information is here in case there is an issue, or to see when was this build was made.
+        <br><br>Build from: <code>25/11/2023</code>
+        <br>Build type: <code>Stable</code></p>
+    </div>
+
+    <!-- Dependencies -->
+    <div class="dependencies">
+        <h1>Dependencies</h1>
+        <div class="dependency-item"><b>vue</b> - 3.3.9</div>
+        <div class="dependency-item"><b>vite</b> - 5.0.2</div>
+        <div class="divider"></div>
+        <div class="dependency-item"><b>inter-font</b> - 4.0.0</div>
+        <div class="dependency-item"><b>geist-font</b> - 1.0.1</div>
+        <div class="dependency-item"><b>axios</b> - 1.6.1</div>
+        <div class="dependency-item"><b>vue-i18n</b> - 9.7.1</div>
+        <div class="dependency-item"><b>vue-router</b> - 4.2.5</div>
+    </div>
+    <!-- Aeolus Card -->
+    <a class="aeolus-card" href="https://gabs.eu.org/aeolus">
+      <div class="aeolus-card-elements">
+      <h1>Aeolus</h1>
+      <p>Made with <span id="heart">♥</span> by <span id="name">Gabs</span></p>
+      </div>
+    </a>
+</div>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+mounted() {
+document.addEventListener('keydown', this.handleEscKeyInfo);
+
+},
+
+beforeDestroy() {
+document.removeEventListener('keydown', this.handleEscKeyInfo);
+
+},
+methods: {
+closeInfoPopup() {
+    this.$router.push('/');
+},
+handleEscKeyInfo(event) {
+  if (event.key === 'Escape') {
+    this.closeInfoPopup();
+  }
+},
+}
+}
+</script>
