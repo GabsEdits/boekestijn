ed43f171c278cbd9a04934d683a1ae4441f14be4
Gabriel Cozma <me@gabs.eu.org>
2024-03-27 19:54:50 +0200

    misc: Update vite dependency version

--- src/website.config.js
+++ src/website.config.js
@@ -3,7 +3,7 @@ export default {
   buildFrom : new Date().toLocaleString(),
   dependency: {
     vue: "3.4.21",
-    vite: "5.2.2",
+    vite: "5.2.6",
     leaflet: "1.9.4",
     axios: "1.6.7",
     vuei18n: "9.9.1",
