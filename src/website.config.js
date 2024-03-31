fe2485f3348bcdf0e8fef980283ae5435eab67c2
Gabriel Cozma <me@gabs.eu.org>
2024-03-31 15:52:29 +0300

    misc: Updated vue-i18n

--- src/website.config.js
+++ src/website.config.js
@@ -6,7 +6,7 @@ export default {
     vite: "5.2.6",
     leaflet: "1.9.4",
     axios: "1.6.7",
-    vuei18n: "9.9.1",
+    vuei18n: "9.10.2",
     router: "4.3.0",
     inter: "4.0.1",
     geist: "1.0.1",
