bdc0bfb3a16eccbb40f34a1da84ac7d05194acfa
Gabriel Cozma <me@gabs.eu.org>
2024-05-04 12:17:58 +0300

    cleanup: Update Deps

--- src/website.config.ts
+++ src/website.config.ts
@@ -2,12 +2,12 @@ export default {
   buildtype: "Next",
   buildFrom : new Date().toLocaleString(),
   dependency: {
-    vue: "3.4.21",
-    vite: "5.2.8",
+    vue: "3.4.26",
+    vite: "5.2.11",
     leaflet: "1.9.4",
-    axios: "1.6.7",
-    vuei18n: "9.11.0",
-    router: "4.3.0",
-    aploe: "0.6.0",
+    axios: "1.6.8",
+    vuei18n: "9.13.1",
+    router: "4.3.2",
+    aploe: "0.6.1",
   },
 };
