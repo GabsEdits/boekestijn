3c6dca89850a1762f4fb20f6103badaa53210d51
Gabriel Cozma <me@gabs.eu.org>
2024-05-06 20:53:57 +0300

    cleanup: Fix faq grid

--- src/website.config.ts
+++ src/website.config.ts
@@ -1,13 +1,13 @@
 export default {
-  buildtype: "Next",
-  buildFrom : new Date().toLocaleString(),
+  buildtype: "Release Candidate",
+  buildFrom : "06/05/2024, 20:49 UTC+3",
   dependency: {
     vue: "3.4.26",
     vite: "5.2.11",
+    aploe: "0.6.2",
     leaflet: "1.9.4",
     axios: "1.6.8",
     vuei18n: "9.13.1",
     router: "4.3.2",
-    aploe: "0.6.1",
   },
 };
