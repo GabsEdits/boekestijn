d374d06fa1626de9042ad88292b144776345b8cc
Gabriel Cozma <me@gabs.eu.org>
2024-05-24 18:17:38 +0300

    cleanup: Update Axios Version in config

--- src/website.config.ts
+++ src/website.config.ts
@@ -1,12 +1,12 @@
 export default {
   buildtype: "Development",
-  buildFrom: "22/05/2024, 10:13 UTC+3",
+  buildFrom: "24/05/2024, 18:17 UTC+3",
   dependency: {
     vue: "3.4.27",
     vite: "5.2.11",
     aploe: "0.9.6",
     leaflet: "1.9.4",
-    axios: "1.6.8",
+    axios: "1.7.2",
     vuei18n: "9.13.1",
     router: "4.3.2",
   },
