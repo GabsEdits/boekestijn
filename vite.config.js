b48462df49994f330da8ee62bacb616db3562b80
Gabriel Cozma <me@gabs.eu.org>
2024-06-01 11:50:08 +0300

    fix(config): update sitemap to the new domain
    Why didn't I do this already?

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { fileURLToPath, URL } from "node:url";
+import { URL, fileURLToPath } from "node:url";
 
+import Sitemap from "vite-plugin-sitemap";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import Sitemap from "vite-plugin-sitemap";
 import zipPack from "vite-plugin-zip-pack";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    Sitemap({ hostname: "https://boekestijn.gabs.eu.org" }),
+    Sitemap({ hostname: "https://boekestijn.gxbs.me" }),
     zipPack({ outDir: "zip", outFileName: "boekestijn.zip" }),
   ],
   resolve: {
