d2f49bcd39b2e108741ea49dfe07e4ac07594816
Gabriel Cozma <me@gabs.eu.org>
2024-07-03 13:15:35 +0300

    cleanup: bump vite

--- src/website.config.ts
+++ src/website.config.ts
@@ -1,9 +1,9 @@
 export default {
   buildtype: "Development",
-  buildFrom: "30/06/2024, 21:28 UTC+3",
+  buildFrom: "03/07/2024, 13:14 UTC+3",
   dependency: {
     vue: "3.4.31",
-    vite: "5.3.2",
+    vite: "5.3.3",
     aploe: "0.9.6",
     leaflet: "1.9.4",
     axios: "1.7.2",
