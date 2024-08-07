ca6c97786ad2650546655257f11431d3c99044f7
Gabriel Cozma <me@gabs.eu.org>
2024-08-07 23:20:14 +0300

    chore: update vue

--- src/website.config.ts
+++ src/website.config.ts
@@ -2,7 +2,7 @@ export default {
   buildtype: "Development",
   buildFrom: "03/07/2024, 13:14 UTC+3",
   dependency: {
-    vue: "3.4.35",
+    vue: "3.4.36",
     vite: "5.3.5",
     aploe: "0.9.6",
     leaflet: "1.9.4",
