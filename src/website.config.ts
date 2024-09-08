14d6ac3ba7fe062931fed5a577d449af9f375161
Gabriel Cozma <me@gabs.eu.org>
2024-09-08 17:37:15 +0300

    cleanup: update vite

--- src/website.config.ts
+++ src/website.config.ts
@@ -3,7 +3,7 @@ export default {
   buildFrom: "14/08/2024, 21:09 UTC+3",
   dependency: {
     vue: "3.4.38",
-    vite: "5.4.2",
+    vite: "5.4.3",
     aploe: "1.0.0-rc.0",
     leaflet: "1.9.4",
     axios: "1.7.7",
