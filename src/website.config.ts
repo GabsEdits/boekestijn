ac5ed4f8a546ea344d92972e3ed8eded410e3495
Gabriel Cozma <me@gabs.eu.org>
2024-10-05 19:52:26 +0300

    cleanup: update developer info

--- src/website.config.ts
+++ src/website.config.ts
@@ -1,10 +1,10 @@
 export default {
   buildtype: "Development",
-  buildFrom: "12/09/2024, 22:42 UTC+3",
+  buildFrom: "05/10/2024, 19:53 UTC+3",
   dependency: {
     vue: "3.5.6",
     vite: "5.4.3",
-    aploe: "1.0.0-rc.0",
+    aploe: "1.0.0-rc.4",
     leaflet: "1.9.4",
     axios: "1.7.7",
     vuei18n: "10.0.3",
